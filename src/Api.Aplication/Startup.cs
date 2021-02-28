34f385adcfed887843d83424ac558b973311f38e
Romario de M Damasceno <romario.auth@gmail.com>
2021-02-27 23:32:54 -0300

    Implementar Swagger

--- src/Api.Aplication/Startup.cs
+++ src/Api.Aplication/Startup.cs
@@ -4,6 +4,8 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Swashbuckle.AspNetCore.Swagger;
+using Microsoft.OpenApi.Models;
 
 
 namespace Aplication
@@ -23,6 +25,20 @@ namespace Aplication
             ConfigureService.ConfigureDependenciesService(services);
             ConfigureRepository.ConfigureDependenciesRepository(services);
             services.AddControllers();
+            services.AddSwaggerGen( c =>
+            {
+                c.SwaggerDoc("v1",
+                    new OpenApiInfo
+                    {
+                        Version = "v1",
+                        Title = "ASP.NET Core 3.1 C# API REST com arquitetura DDD na Prática",
+                        Description = "Arquitetura DDD",
+                        Contact = new OpenApiContact
+                        {
+                            Name = "Romario de Melo Damasceno",
+                        }
+                    });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -33,6 +49,14 @@ namespace Aplication
                 app.UseDeveloperExceptionPage();
             }
 
+            // Ativando middlewares para uso do Swagger
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.RoutePrefix = string.Empty;
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "API com AspNetCore 3.1");
+            });
+            
             app.UseRouting();
 
             app.UseAuthorization();
